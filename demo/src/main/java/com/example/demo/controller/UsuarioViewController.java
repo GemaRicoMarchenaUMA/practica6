e6322bdacbdf84fa074e61eb9b59429f5849ad8b
marwa-am <marwa.am@uma.es>
2025-12-15 00:00:16 +0100

    Mejora del header a pantalla completa y actualización de imagen

--- demo/src/main/java/com/example/demo/controller/UsuarioViewController.java
+++ demo/src/main/java/com/example/demo/controller/UsuarioViewController.java
@@ -15,20 +15,20 @@ public class UsuarioViewController {
 	@Autowired
     private UsuarioService usuarioService;
 	
-	/*---Devuelve el formulario de index de usuarios   ---*/
+	//Devuelve el formulario de index de usuarios  
 	@GetMapping("/")
 	public String indexUsuarioView() {
 		return "indexU";
 	}
 
-	/*---Devuelve el formulario para anyadir una nueva cuenta con una cuenta vacia---*/
+	//Devuelve el formulario para anyadir una nueva cuenta con una cuenta vacia
 	@GetMapping("/addUsuario")
 	public String addUsuarioView(Model model) {
 		model.addAttribute("usuario", new Usuario());
 		return "addUsuario";
 	}
 
-	/*---Devuelve el formulario para listar las cuentas del sistema---*/
+	//Devuelve el formulario para listar las cuentas del sistema
 	@GetMapping("/listUsuario")
 	public String listUsuarioView(Model model) {
 		
@@ -38,7 +38,7 @@ public class UsuarioViewController {
 	}
 
 
-	/*---Devuelve el formulario para editar una Cuenta---*/
+	//Devuelve el formulario para editar una Cuenta
 	@GetMapping("/editUsuario/{id}")
 	public String editUsuarioView(@PathVariable("id") Long id, Model model) {
 
